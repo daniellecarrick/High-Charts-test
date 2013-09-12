2365b03d15b19c496ce0763699b6e4c649ee3b03
daniellecarrick <dacarrick@gmail.com>
2013-09-12 16:16:11 -0400

    Dan saves the day!
    bold labeling, sliding animation on arrow click, intro slide

--- js/main.js
+++ js/main.js
@@ -13,8 +13,12 @@ $(function () {
 			
 		//}
 	//}
-	 function ScrollBy100 () {
-            window.scrollBy (0,100);}
+	$( "#right-button" ).click(function() {
+		$('body').animate({scrollLeft : $('body').scrollLeft() + 300});
+	});
+	$( "#left-button" ).click(function() {
+		$('body').animate({scrollLeft : $('body').scrollLeft() -300});
+	});
 	
 	var events = {
 		1974 : {
@@ -67,130 +71,161 @@ $(function () {
 		}
 	}
 
-		$('#container').highcharts({
-			chart: {
-				width:3000,
-				height:400,
-				backgroundColor : '#0b1112'
-			},
+	$('#container').highcharts({
+		chart: {
+			width:3000,
+			height:400,
+			backgroundColor : '#0b1112'
+		},
 
-			colors: [
-				   '#e52423', 
-				   '#f6a738', 
-				   '#B4E038', 
-				   '#007dc3', 
-				   ],
+		colors: [
+			   '#e52423', 
+			   '#f6a738', 
+			   '#B4E038', 
+			   '#007dc3', 
+			   ],
 
-			title: {
-				text: '',
-			   
-			},
+		title: {
+			text: '',
+		   
+		},
 
-			xAxis: { 
-				type: 'datetime',
-				opposite: true,
-				labels: {
-					rotation: 270,
-					style: {color: '#fff'}
-				},
-				tickWidth: 0,
-				lineWidth: 0
+		xAxis: { 
+			type: 'datetime',
+			opposite: true,
+			labels: {
+				rotation: 270,
+				style: {color: '#fff'}
 			},
+			tickWidth: 0,
+			lineWidth: 0
+		},
 
-			yAxis: {
-				title: {text: ''},
-				min: 0,
-				max: 80,
-				gridLineWidth: 0,
-				labels: {enabled: false},
-				plotLines: [{ 
-                color: 'white',
-                width: 2,
-                value: 0,
-            }],
-			},
-			/*plotOptions: {
-                series: {
-                    point: {
-                        events: {
-                            mouseOver: function() {
-                            	$(this.series.chart.xAxis[0].labelGroup.element.childNodes[this.x]).css('fill', 'black');
-                            },
-                            mouseOut: function() {                       
-                                 $(this.series.chart.yAxis[0].labelGroup.element.childNodes[this.x]).css('fill', '#999999');
-                            }
+		yAxis: {
+			title: {text: ''},
+			min: 0,
+			max: 80,
+			gridLineWidth: 0,
+			labels: {enabled: false},
+			plotLines: [{ 
+            color: 'white',
+            width: 2,
+            value: 0,
+        }],
+		},
+		/*plotOptions: {
+            series: {
+                point: {
+                    events: {
+                        mouseOver: function() {
+                        	$(this.series.chart.xAxis[0].labelGroup.element.childNodes[this.x]).css('fill', 'black');
+                        },
+                        mouseOut: function() {                       
+                             $(this.series.chart.yAxis[0].labelGroup.element.childNodes[this.x]).css('fill', '#999999');
                         }
                     }
                 }
-            },*/
-			tooltip: {
-				backgroundColor: '#fff',
-				borderColor: 'black',
-				borderRadius: 2,
-				borderWidth: 0,
-				width: 400,
-				crosshairs: true,
-				useHTML : true,
-				formatter: function() {
-					if(events[yearConverter(this.x)]){
-						return '<p><b>' + yearConverter(this.x) + '</b></br>' + this.series.name + ':' + this.y + '%' + events[yearConverter(this.x)].content;
-					}
-					else {
-						return '<p><b>' + yearConverter(this.x) + '</b></br>' + this.series.name + ':' + this.y + '%';
-					}
+            }
+        },*/
+		tooltip: {
+			backgroundColor: '#fff',
+			borderColor: 'black',
+			borderRadius: 2,
+			borderWidth: 0,
+			width: 400,
+			crosshairs: true,
+			useHTML : true,
+			formatter: function() {
+				if(events[yearConverter(this.x)]){
+					return '<p><b>' + yearConverter(this.x) + '</b></br>' + this.series.name + ':' + this.y + '%' + events[yearConverter(this.x)].content;
 				}
-			},
+				else {
+					return '<p><b>' + yearConverter(this.x) + '</b></br>' + this.series.name + ':' + this.y + '%';
+				}
+			}
+		},
 
-			legend: {
-				borderWidth: 0,
-				layout: 'vertical',
-				align: 'left',
-				itemHiddenStyle: {color: '#fff'},
-				itemStyle: {color: '#fff'},
-				itemHoverStyle: {
-					color: '#fff',
-					fontWeight: 'bold'
-					},
-				floating: true,
-				y: -350
-			},
-			series: [{
-				name: 'Environmental Pollution (US)',
-				data: [12,11,11,13,10,10,10,7,8,12,14,13,15,14,19,21,18,16,15,12,13,12,12,16,16,17,17,14,12,12,13,13,12,15,19,18,15,16,17],
-				pointStart: Date.UTC(1974, 0, 1),
-				pointInterval: 24 * 3600 *1000 * 365, // one year
-				marker: {
-					symbol: 'circle',
-					states: {
-					   hover: {
-						fillColor: '#0b1112',
-					   }
-					}
+		legend: {
+			borderWidth: 0,
+			layout: 'vertical',
+			align: 'left',
+			itemHiddenStyle: {color: '#fff'},
+			itemStyle: {color: '#fff'},
+			itemHoverStyle: {
+				color: '#fff',
+				fontWeight: 'bold'
+				},
+			floating: true,
+			y: -350
+		},
+		series: [{
+			name: 'Environmental Pollution (US)',
+			data: [12,11,11,13,10,10,10,7,8,12,14,13,15,14,19,21,18,16,15,12,13,12,12,16,16,17,17,14,12,12,13,13,12,15,19,18,15,16,17],
+			pointStart: Date.UTC(1974, 0, 1),
+			pointInterval: 24 * 3600 *1000 * 365, // one year
+			marker: {
+				symbol: 'circle',
+				states: {
+				   hover: {
+					fillColor: '#0b1112',
+				   }
 				}
-			}, {
-				name: 'Recession and Unemployment (US)',
-				data: [15,33,20,19,20,27,24,34,50,22,20,17,17,16,12,10,26,41,28,21,16,19,15,15,8,6,17,24,25,24,19,21,19,22,56,56,50,53,46],
-				pointStart: Date.UTC(1974, 0, 1),
-				pointInterval: 24 * 3600 *1000 * 365, // one year
-				marker: {
-					symbol: 'circle'
-				}
-			}, {
-				name: 'Environmental Pollution (Global)',
-				data: [23,25,28,26,26,26,25,24],
-				pointStart: Date.UTC(2006, 0, 1),
-				pointInterval: 24 * 3600 *1000 * 365, // one year
-				marker: {
-					symbol: 'circle'
-				}
-			}, {
-				name: 'Recession and Unemployment (Global)',
-				data: [37,34,33,51,47,42,44,41],
-				pointStart: Date.UTC(2006, 0, 1),
-				pointInterval: 24 * 3600 *1000 * 365, // one year
-				marker: {
-					symbol: 'circle'
-				}
-			}]
-		});
+			}
+		}, {
+			name: 'Recession and Unemployment (US)',
+			data: [15,33,20,19,20,27,24,34,50,22,20,17,17,16,12,10,26,41,28,21,16,19,15,15,8,6,17,24,25,24,19,21,19,22,56,56,50,53,46],
+			pointStart: Date.UTC(1974, 0, 1),
+			pointInterval: 24 * 3600 *1000 * 365, // one year
+			marker: {
+				symbol: 'circle'
+			}
+		}, {
+			name: 'Environmental Pollution (Global)',
+			data: [23,25,28,26,26,26,25,24],
+			pointStart: Date.UTC(2006, 0, 1),
+			pointInterval: 24 * 3600 *1000 * 365, // one year
+			marker: {
+				symbol: 'circle'
+			}
+		}, {
+			name: 'Recession and Unemployment (Global)',
+			data: [37,34,33,51,47,42,44,41],
+			pointStart: Date.UTC(2006, 0, 1),
+			pointInterval: 24 * 3600 *1000 * 365, // one year
+			marker: {
+				symbol: 'circle'
+			}
+		}]
 	});
+	
+	//add classes to labels with events
+	var chartLabels = $('#container').find('.highcharts-axis-labels').find('tspan');
+
+	chartLabels.each(function(index){
+		var labelText = $(this).text();
+		if(events[labelText]){
+			$(this).addClass('active-label');
+		}
+	});
+
+	//slide functionality
+	$(window).on({
+		resize : function(){
+			$('.chart-wrapper').css({left : $(window).outerWidth(true)});
+		}
+	});
+	$(window).trigger('resize');
+
+	$('.js-enter').on({
+		click : function(){
+			$('body').animate({scrollLeft : $(window).outerWidth(true)}, 500, function(){
+				$('.arrow').fadeIn();
+			});
+		}
+	});
+
+
+});
+
+
+
